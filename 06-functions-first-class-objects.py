70d07f3195494f184db5fa2f22c2ef528b419fbe
Aditya <sp.aditya@gmail.com>
2019-08-07 01:21:37 +0530

    Add files via upload

--- 06-functions-first-class-objects.py
+++ 06-functions-first-class-objects.py
@@ -118,3 +118,113 @@ NameError: name 'l' is not defined
 >>> sorted(li, key=max)
 [[5, 6, 7], [0, 8, 9], [9, 0, 0]]
 >>> 
+
+>>> 
+>>> 
+>>> 3
+3
+>>> 'Hello'
+'Hello'
+>>> add(3,4)
+7
+>>> x = 3
+>>> y = 4
+>>> add(x,y)
+7
+>>> def toupper(s):
+	return s.upper()
+
+>>> sorted(li, key=strlen)
+[[5, 6, 7], [0, 8, 9], [9, 0, 0]]
+>>> def toupper(s):
+	return s.upper()
+
+>>> li= ['Adam', 'adrian','ben','John']
+>>> # function literals or anonymous functions
+>>> def add(x,y):
+	return x + y
+
+>>> 
+>>> 
+>>> calc(add, 4,5)
+9
+>>> def add(x,y):
+	return x + y
+
+>>> lambda x,y: x+y
+<function <lambda> at 0x000001CC8F1713A8>
+>>> d = lambda x,y: x+y
+>>> d(8,9)
+17
+>>> def calc(f, x, y):
+	return f(x,y)
+
+>>> calc(lambda x,y: x +y, 3,4)
+7
+>>> calc(lambda x,y: x**2 +y**2, 3,4)
+25
+>>> sorted(li,key = lambda x: x.upper())
+['Adam', 'adrian', 'ben', 'John']
+>>> li = [[5,6,7],[0,8,9],[9,0,0]]
+>>> 
+>>> 
+>>> sorted(li)
+[[0, 8, 9], [5, 6, 7], [9, 0, 0]]
+>>> sorted(li, key = lambda x: x[1])
+[[9, 0, 0], [5, 6, 7], [0, 8, 9]]
+>>> li = [[5,6,7],[0,0,9],[9,0,0]]
+>>> sorted(li, key = lambda x: x[1])
+[[0, 0, 9], [9, 0, 0], [5, 6, 7]]
+>>> # map and filter
+>>> 
+>>> 
+>>> li = [2,3,4.5]
+>>> newli = []
+>>> for sal in li:
+	newli.appen(sal + sal * 0.15)
+
+	
+Traceback (most recent call last):
+  File "<pyshell#121>", line 2, in <module>
+    newli.appen(sal + sal * 0.15)
+AttributeError: 'list' object has no attribute 'appen'
+>>> for sal in li:
+	newli.append(sal + sal * 0.15)
+
+	
+>>> newli
+[2.3, 3.45, 5.175]
+>>> map(lambda x : x + x * 0.15, li)
+<map object at 0x000001CC8F118508>
+>>> list(map(lambda x : x + x * 0.15, li))
+[2.3, 3.45, 5.175]
+>>> sum(map(lambda x : x + x * 0.15, li))
+10.925
+>>> li = [3,4,5]
+>>> list(map(lambda x : x*x , li))
+[9, 16, 25]
+>>> 
+>>> 
+>>> ages = [12,34,56,78,89]
+>>> list(filter(lambda x: 30<x<60, ages))
+[34, 56]
+>>> filter(lambda x: 30<x<60, ages)
+<filter object at 0x000001CC8F14BEC8>
+>>> sum(filter(lambda x: 30<x<60, ages))
+90
+>>> min(filter(lambda x: 30<x<60, ages))
+34
+>>> max(filter(lambda x: 30<x<60, ages))
+56
+>>> list(filter(lambda x: 30<x<60, ages))
+[34, 56]
+>>> s = '23,45,56'.split(',')
+>>> s
+['23', '45', '56']
+>>> list(map(int, s))
+[23, 45, 56]
+>>> sum(map(int,s))
+124
+>>> 
+>>> 
+>>> 
---
+++ prog/adder_fun.py
@@ -0,0 +1,4 @@
+ui = input("Enter comma seperated numbers: ").split(',')
+print("The sum is", sum(map(int,ui)))
+
+print (sum([int(i) for i in ui]))
