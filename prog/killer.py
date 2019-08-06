8df2f6da9cb5b696e9bb2f40ce4b7ffe18a02e9a
Aditya <sp.aditya@gmail.com>
2019-08-07 02:17:33 +0530

    Add files via upload

--- prog/killer.py
+++ prog/killer.py
@@ -1,25 +1,38 @@
 import subprocess
 
-output = subprocess.check_output("tasklist", shell=True).decode()
-#print(output[:200])
+output = subprocess.check_output("tasklist", shell=True).decode().split("\n")[3:-1]
 
-output_lines = output.split("\n")[3:-1]
-#print(output_lines[:5])
-
-
-edge = list(filter(lambda line: 'MicrosoftEdge' in line, output_lines))
-#print(edge)
-edge = [line for line in output_lines if 'MicrosoftEdge' in line]
-#print(edge)
-
-pids = list(map(lambda x : x.split()[1], edge))
-#print(pids)
-pids = [ line.split()[1] for line in edge]
-#print(pids)
-
+pids = [line.split()[1] for line in output if 'MicrosoftEdge' in line]
 
 for pid in pids:
     subprocess.call("taskkill /F /PID " + pid, shell=True)
+
+
+#tasklist 
+
+#VERSION 1
+#import subprocess
+#
+#output = subprocess.check_output("tasklist", shell=True).decode()
+##print(output[:200])
+#
+#output_lines = output.split("\n")[3:-1]
+##print(output_lines[:5])
+#
+#
+#edge = list(filter(lambda line: 'MicrosoftEdge' in line, output_lines))
+##print(edge)
+#edge = [line for line in output_lines if 'MicrosoftEdge' in line]
+##print(edge)
+#
+#pids = list(map(lambda x : x.split()[1], edge))
+##print(pids)
+#pids = [ line.split()[1] for line in edge]
+##print(pids)
+#
+#
+#for pid in pids:
+#    subprocess.call("taskkill /F /PID " + pid, shell=True)
 #tasklist 
 #ps -ef
 
