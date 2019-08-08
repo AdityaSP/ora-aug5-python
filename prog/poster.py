534145400be207dd2e375f8a6e7f596613d6be86
AdityaSP <sp.aditya@gmail.com>
2019-08-09 03:23:44 +0530

    poster download

--- prog/poster.py
+++ prog/poster.py
@@ -1,10 +1,20 @@
 import json
-fp = r'D:\training\python\ora-aug5-python\prog\json.data'
-fh = open(fp, 'rt')
-jsonstr = fh.read()
-movie_data = json.loads(jsonstr)
-print(type(movie_data))
-print(movie_data.keys())
+#fp = r'D:\training\python\ora-aug5-python\prog\json.data'
+#fh = open(fp, 'rt')
+#jsonstr = fh.read()
+#movie_data = json.loads(jsonstr)
+import requests
+r = requests.get('http://www.omdbapi.com/?s=spiderman&apikey=b4e17ea0')
 
-for movie in movie_data['Search']:
-    print(movie['Poster'])
+if r.ok:
+    movie_data=r.json()
+
+    for movie in movie_data['Search']:
+        if movie['Poster'] != 'N/A':
+            print ("Downloading", movie['Poster'])
+            poster = requests.get(movie['Poster'])
+            if poster.ok:
+                fh = open(movie['imdbID'] +'.jpg' ,'wb') 
+                fh.write(poster.content)
+                fh.close()
+                
